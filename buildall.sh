2d6bb2fb8fe36bd1e560957778875792283ca1f4
Radomír Černoch <radomir.cernoch@gmail.com>
2017-07-18 09:37:32 +0200

    Skip static build on OS-X

--- buildall.sh
+++ buildall.sh
@@ -5,10 +5,20 @@ for DIR in core simp
 do
   cd $DIR
 
-  for BIN in s p d r rs libs libp libd libr
+  for BIN in p d r rs
   do
     make $BIN
   done
+
+  for BIN in debug profile release standard
+  do
+    make lib_$BIN.a
+  done
+
+  if [[ "$TRAVIS_OS_NAME" != "osx" ]]
+  then
+    make s
+  fi
 
   cd ..
 done
