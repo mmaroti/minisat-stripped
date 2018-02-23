033c233daa5d9498c6fb1c8b2bdc73ed5b96436f
Radomír Černoch <radomir.cernoch@gmail.com>
2018-02-23 14:30:20 +0100

    Build script attempts C++ 1998, 2011 and 2014

--- build.sh
+++ build.sh
@@ -1,24 +1,45 @@
 #!/bin/bash -e
 export MROOT=`pwd`
 
-for DIR in core simp
+for STD in c++98 c++11 c++14
 do
-  cd minisat/$DIR
-
-  for BIN in s p d r
-  do
-    make $BIN
-  done
-
-  for BIN in debug profile release standard
-  do
-    make lib_$BIN.a
-  done
-
-  if [[ "$TRAVIS_OS_NAME" != "osx" ]]
+  export CFLAGS="-std=$STD -Wall -Wno-parentheses -Wextra"
+  
+  if [ "$STD" != c++98 ]
   then
-    make rs
+    export CFLAGS="$CFLAGS -Wmissing-noreturn"
+  fi
+  
+  if [ "$CXX" == clang++ ]
+  then
+    export CFLAGS="$CFLAGS -Wweak-vtables"
   fi
 
-  cd ../..
+  for DIR in core simp
+  do
+    echo =======================================
+    echo ===== Building \'$DIR\' under $STD =====
+    echo =======================================
+
+    cd minisat/$DIR
+    make clean
+    rm -f *.a
+
+    for BIN in s p d r
+    do
+      make $BIN
+    done
+
+    for BIN in debug profile release standard
+    do
+      make lib_$BIN.a
+    done
+
+    if [[ "$TRAVIS_OS_NAME" != "osx" ]]
+    then
+      make rs
+    fi
+
+    cd ../..
+  done
 done
