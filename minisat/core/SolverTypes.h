c3fed47330402d1b1018075146c236ce7252e3b7
Miklos Maroti <mmaroti@gmail.com>
2018-10-21 17:15:59 +0200

    cleanup and making lbool simpler

--- minisat/core/SolverTypes.h
+++ minisat/core/SolverTypes.h
@@ -1,44 +1,44 @@
-/***********************************************************************************[SolverTypes.h]
+/******************************************************************************
 Copyright (c) 2003-2006, Niklas Een, Niklas Sorensson
 Copyright (c) 2007-2010, Niklas Sorensson
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute,
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or
-substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
-OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-**************************************************************************************************/
-
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+******************************************************************************/
 
 #ifndef Minisat_SolverTypes_h
 #define Minisat_SolverTypes_h
 
-#include <cassert>
 #include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <iostream>
 #include <vector>
-
-#include "minisat/mtl/Vec.h"
 
 namespace Minisat {
 
-//=================================================================================================
-// Variables, literals, lifted booleans, clauses:
-
-
-// NOTE! Variables are just integers. No abstraction here. They should be chosen from 0..N,
-// so that they can be used as array indices.
+//=============================================================================
+// Variables
 
 typedef int Var;
 constexpr Var var_Undef(-1);
+
+//=============================================================================
+// Literals
 
 class Lit {
 protected:
@@ -49,7 +49,8 @@ protected:
 public:
   Lit() = default;
   constexpr Lit(const Lit &) = default;
-  explicit constexpr Lit(Var var, bool sign = false) : x(var + var + (int)sign) {}
+  explicit constexpr Lit(Var var, bool sign = false)
+      : x(var + var + (int)sign) {}
 
   constexpr bool operator==(Lit p) const { return x == p.x; }
   constexpr bool operator!=(Lit p) const { return x != p.x; }
@@ -67,52 +68,37 @@ public:
 constexpr Lit lit_Undef(var_Undef, false);
 constexpr Lit lit_Error(var_Undef, true);
 
-//=================================================================================================
+//=============================================================================
 // Lifted booleans:
-//
-// NOTE: this implementation is optimized for the case when comparisons between values are mostly
-//       between one variable and one constant. Some care had to be taken to make sure that gcc
-//       does enough constant propagation to produce sensible code, and this appears to be somewhat
-//       fragile unfortunately.
-
-#define l_True  (lbool((uint8_t)0)) // gcc does not do constant propagation if these are real constants.
-#define l_False (lbool((uint8_t)1))
-#define l_Undef (lbool((uint8_t)2))
 
 class lbool {
-    uint8_t value;
+  uint8_t value;
 
 public:
-    explicit lbool(uint8_t v) : value(v) { }
+  constexpr explicit lbool(uint8_t v) : value(v) {}
 
-    lbool()       : value(0) { }
-    explicit lbool(bool x) : value(!x) { }
+  constexpr lbool() : value(0) {}
+  explicit constexpr lbool(bool x) : value(!x) {}
 
-    bool  operator == (lbool b) const { return !!(((b.value&2) & (value&2)) | (!(b.value&2)&(value == b.value))); }
-    bool  operator != (lbool b) const { return !(*this == b); }
-    lbool operator ^  (bool  b) const { return lbool((uint8_t)(value^(uint8_t)b)); }
+  bool operator==(lbool b) const {
+    return (value & b.value & 2) != 0 || value == b.value;
+  }
 
-    bool  isTrue()  const { return *this == l_True;  }
-    bool  isFalse() const { return *this == l_False; }
+  bool operator!=(lbool b) const { return !(*this == b); }
+  constexpr lbool operator^(bool b) const {
+    return lbool((uint8_t)(value ^ (uint8_t)b));
+  }
 
-    lbool operator && (lbool b) const {
-        uint8_t sel = (this->value << 1) | (b.value << 3);
-        uint8_t v   = (0xF7F755F4 >> sel) & 3;
-        return lbool(v); }
-
-    lbool operator || (lbool b) const {
-        uint8_t sel = (this->value << 1) | (b.value << 3);
-        uint8_t v   = (0xFCFCF400 >> sel) & 3;
-        return lbool(v); }
-
-    friend int   toInt  (lbool l);
-    friend lbool toLbool(int   v);
+  constexpr bool isTrue() const { return value == 0; }
+  constexpr bool isFalse() const { return value == 1; }
 };
-inline int   toInt  (lbool l) { return l.value; }
-inline lbool toLbool(int   v) { return lbool((uint8_t)v);  }
 
-//=================================================================================================
-// Clause -- a simple class for representing a clause:
+constexpr lbool l_True((uint8_t)0);
+constexpr lbool l_False((uint8_t)1);
+constexpr lbool l_Undef((uint8_t)2); // 3 is also undef
+
+//============================================================================
+// Clause:
 
 class Clause;
 typedef Clause *CRef;
@@ -146,18 +132,19 @@ public:
   }
 };
 
-//=================================================================================================
-// Watcher (moved here from Solver.h)
+//============================================================================
+// Watcher:
 
 struct Watcher {
-    CRef cref;
-    Lit  blocker;
-    Watcher(CRef cr, Lit p) : cref(cr), blocker(p) {}
-    bool operator==(const Watcher& w) const { return cref == w.cref; }
-    bool operator!=(const Watcher& w) const { return cref != w.cref; }
+  CRef cref;
+  Lit blocker;
+
+  Watcher(CRef cr, Lit p) : cref(cr), blocker(p) {}
+
+  bool operator==(const Watcher &w) const { return cref == w.cref; }
+  bool operator!=(const Watcher &w) const { return cref != w.cref; }
 };
 
-//=================================================================================================
-}
+} // namespace Minisat
 
 #endif
